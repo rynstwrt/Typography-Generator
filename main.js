fe9a261e1ec6be6183d3acf319dd1863e237337d
Ryan Stewart <ryanstewartalex@gmail.com>
2020-10-31 05:59:24 -0500

    Add Drag and Drop

--- main.js
+++ main.js
@@ -1,27 +1,52 @@
+let mouseTimeoutId;
 
-$('input[type=text]').on('input', function()
+async function mouseStep(h1)
 {
-	const line = this.id.replace(/\D/g, '');
-	$(`#line${line}`).text(this.value);
+	mouseTimeoutId = setTimeout(() =>
+	{
+
+	}, 500);
+}
+
+function deselectAll()
+{
+	const t = $('h1');
+	t.removeClass('selected');
+	t.unbind('mousemove');
+}
+
+$('h1').click(function()
+{
+	const t = $(this);
+
+	if (t.hasClass('selected'))
+	{
+		t.removeClass('selected');
+		deselectAll();
+		return;
+	}
+
+	t.addClass('selected');
+	t.css({'position': 'absolute'});
+
+	const squares = $(`
+		<div id='square1' class='selectedsquare'></div>
+		<div id='square2' class='selectedsquare'></div>
+		<div id='square3' class='selectedsquare'></div>
+		<div id='square4' class='selectedsquare'></div>
+	`);
+	t.append(squares);
+
+	t.on('mousemove', (e) =>
+	{
+		const cursorX = e.clientX;
+		const cursorY = e.clientY;
+		t.css({'left': cursorX - (t.width() / 2)});
+		t.css({'top': cursorY - (t.height() / 2)});
+	});
 });
 
-$('input[type=range]').on('input', function()
+$('#container').children().not('h1').click(function()
 {
-	const line = this.id.replace(/\D/g, '');
-	const isFontSize = this.id.includes('fontsize');
-	const isWidth = this.id.includes('width');
-	const isHeight = this.id.includes('height');
-	const isX = this.id.includes('horizontal');
-	const isY = this.id.includes('vertical');
-
-	if (isFontSize)
-		$(`#line${line}`).css({'font-size': `${this.value}rem`});
-	else if (isWidth)
-		$(`#line${line}`).css({'letter-spacing': `${this.value}rem`});
-	else if (isHeight)
-		$(`#line${line}`).css({'transform': `scaleY(${this.value})`});
-	else if (isX)
-		$(`#line${line}`).css({'margin-left': `${this.value}rem`});
-	else if (isY)
-		$(`#line${line}`).css({'margin-bottom': `${this.value}rem`});
+	deselectAll();
 });
