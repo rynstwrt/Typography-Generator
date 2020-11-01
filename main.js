782451502e322473a2ab519bc51825b4eed2bfdc
Ryan Stewart <ryanstewartalex@gmail.com>
2020-10-31 19:05:19 -0500

    Add Theme and Font Selectors, Reset Button

--- main.js
+++ main.js
@@ -3,9 +3,52 @@ function deselectAll()
 	const t = $('h1');
 	t.removeClass('selected');
 	t.unbind('mousemove');
+	$(document).unbind('keydown');
 }
 
 
+
+$('#fontselect').on('input', function()
+{
+	let font;
+
+	switch ($(this).val())
+	{
+		case 'Anton':
+			font = "'Anton', sans-serif";
+			break;
+		case 'Barlow Condensed':
+			font = "'Barlow Condensed', sans-serif";
+			break;
+		case 'Lato':
+			font = "'Lato', sans-serif";
+			break;
+		case 'Open Sans':
+			font = "'Open Sans', sans-serif";
+			break;
+		case 'Roboto':
+			font = "'Roboto', sans-serif";
+			break;
+		case 'Roboto Condensed':
+			font = "'Roboto Condensed', sans-serif";
+			break;
+	}
+
+	$('h1').css({'font-family': font});
+});
+
+
+$('#themeselect').on('input', function()
+{
+	if ($(this).val() === 'Light')
+	{
+		$('body').addClass('light');
+	}
+	else
+	{
+		$('body').removeClass('light');
+	}
+});
 
 
 
@@ -19,7 +62,7 @@ $('#container').click(function(e)
 
 
 
-
+$('#resetbutton').click(() => $('h1').remove());
 
 
 
@@ -36,7 +79,15 @@ $('#newlinebutton').click(() =>
 	{
 		if (e.target.tagName !== 'H1') return;
 
-		h1.css({'position': 'absolute'});
+		if (h1.css('position') === 'static')
+		{
+			const left = h1.offset().left;
+			const top = h1.offset().top;
+			h1.css({'position': 'absolute'});
+			h1.css({'left': left});
+			h1.css({'top': top});
+		}
+
 		h1.on('mousemove', (e) =>
 		{
 			const cursorX = e.clientX;
@@ -46,6 +97,32 @@ $('#newlinebutton').click(() =>
 		});
 	});
 
+
+	h1.dblclick(function()
+	{
+		$(document).on('keydown', function(e)
+		{
+			const key = e.key;
+			const keyCode = e.keyCode;
+			let currentText = h1.text();
+
+			console.log(keyCode);
+
+			if (e.keyCode === 8) // backspace
+			{
+				currentText = currentText.split('');
+				currentText.pop();
+				currentText = currentText.join('');
+
+				h1.text(currentText);
+				return;
+			}
+			else if (keyCode === 32 || (keyCode >= 65 && keyCode <= 90)) // if space or a-z
+			{
+				h1.text(currentText + key);
+			}
+		});
+	});
 
 
 
@@ -57,6 +134,7 @@ $('#newlinebutton').click(() =>
 			<div id='square4' class='selectedsquare'></div>
 		`);
 		h1.append(squares);
+
 
 		squares.on('mousedown', () =>
 		{
@@ -78,6 +156,10 @@ $('#newlinebutton').click(() =>
 		{
 			squares.unbind('mousemove');
 		});
+
+
+
+
 
 		h1.unbind('mousemove');
 	});
